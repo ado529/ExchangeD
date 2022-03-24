dcce265662e333d5d9e3d5b7db8a4b068e6a8eb0
ado529 <adoado529@yahoo.co.jp>
2022-03-24 16:03:01 +0900

    tree

--- functions.php
+++ functions.php
@@ -2,9 +2,13 @@
 
 function connect_to_db()
 {
-  $dbn = 'mysql:dbname=exchanged;charset=utf8mb4;port=3306;host=localhost';
-  $user = 'root';
-  $pwd = '';
+  $dbn = 'mysql:dbname=heroku_23c399267c9873f;charset=utf8mb4;port=3306;host=us-cdbr-east-05.cleardb.net';
+  $user = 'bf372b7567c4a6';
+  $pwd = 'bd57bb55';
+
+  //$dbn = 'mysql:dbname=exchanged;charset=utf8mb4;port=3306;host=localhost';
+  //$user = 'root';
+  //$pwd = '';
 
   try {
     return new PDO($dbn, $user, $pwd);
